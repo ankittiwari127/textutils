1f25eca9c67b4dd54947429963a2ad7f1f66a10d
ankittiwari127 <118838774+ankittiwari127@users.noreply.github.com>
2023-06-23 08:24:45 +0530

    changed the navbar twice

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,8 +6,7 @@ export default function Navbar(props) {
   <a className="navbar-brand" href="/">{props.title}</a>
   
   <div className="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul className="navbar-nav">
-    </ul>
+    
     <div className={`form-check form-switch text-${props.mode==="light"?"dark":"light"}`}>
   <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.togglemode}/>
   <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable dark mode</label>
